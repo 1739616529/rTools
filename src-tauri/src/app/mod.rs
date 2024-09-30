d5a62374a2b13c691b961560b014341368ed3530
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-09-30 17:56:55 +0800

    chore: log plugin

--- src-tauri/src/app/mod.rs
+++ src-tauri/src/app/mod.rs
@@ -1,16 +1,11 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
-
-mod setup;
 mod plugin;
+mod setup;
 
 use tauri::{generate_context, Builder};
 use tauri_plugin_autostart::MacosLauncher;
-use tauri_plugin_log::{Target, TargetKind};
-use tauri_plugin_window_state::StateFlags;
-
+// use tauri_plugin_log::{Target, TargetKind};
 
 pub const AUTO_LAUNCH_ARG: &str = "--auto-launch";
 
@@ -18,16 +13,12 @@ pub struct Applican;
 
 impl Applican {
     pub fn run() {
-
         #[cfg(target_os = "linux")]
         std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
 
-
         let context = generate_context!();
 
-
-
-        let app = Builder::default()
+        let mut builder = Builder::default()
             .plugin(tauri_plugin_single_instance::init(|_, _, _| {}))
             .setup(setup::init)
             // 托盘
@@ -35,33 +26,32 @@ impl Applican {
             // 快捷键
             .plugin(plugin::shortcut::init())
             // 开机启动
-            .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec![AUTO_LAUNCH_ARG])))
+            .plugin(tauri_plugin_autostart::init(
+                MacosLauncher::LaunchAgent,
+                Some(vec![AUTO_LAUNCH_ARG]),
+            ))
+            // 窗口
+            .plugin(plugin::window::init())
             // 日志信息
-            // .plugin(
-            //     tauri_plugin_log::Builder::default()
-                // .targets([
-                //     Target::new(TargetKind::LogDir { file_name: None }),
-                //     Target::new(TargetKind::Webview),
-                //     Target::new(TargetKind::Stderr),
-                // ])
-                // .level(log::LevelFilter::Info)
-                // .build(),
-            // )
-            // 窗口记忆
             .plugin(
-                tauri_plugin_window_state::Builder::default().with_state_flags(StateFlags::all()).build()
+                tauri_plugin_log::Builder::new()
+                    .build(),
             )
             // .manage(state)
-            .build(context).expect("error while running tauri application");
+            ;
 
 
+
+        #[cfg(debug_assertions)]
+        {
+            let devtools = tauri_plugin_devtools::init(); // initialize the plugin as early as possible
+            builder = builder.plugin(devtools);
+        }
+
+        let app = builder.build(context)
+            .expect("error while running tauri application");
         app.run(|_app, event| match event {
-            tauri::RunEvent::ExitRequested { api, .. } => {
-                api.prevent_exit();
-            }
             _ => {}
         });
-
     }
-
 }
